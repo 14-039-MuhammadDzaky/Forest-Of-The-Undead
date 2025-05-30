e3c3786601ccd65f8260422dbc2f800133dd909d
14-039-MuhammadDzaky <muhammad.123140039@student.itera.ac.id>
2025-05-30 21:52:38 +0700

    Add files via upload

---
+++ data/tilesets/outside.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.1" name="outside" tilewidth="64" tileheight="64" tilecount="256" columns="12">
+ <image source="../../asset/tilesets/outside.png" width="1024" height="1024"/>
+</tileset>
